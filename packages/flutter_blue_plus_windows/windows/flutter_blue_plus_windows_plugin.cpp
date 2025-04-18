b11ace056ec9efdbb4d93009bffd02b712e070a0
Himchan Park <chan150@kaist.ac.kr>
2025-04-18 17:35:12 +0900

    🔨 method channel function들 추가

--- packages/flutter_blue_plus_windows/windows/flutter_blue_plus_windows_plugin.cpp
+++ packages/flutter_blue_plus_windows/windows/flutter_blue_plus_windows_plugin.cpp
@@ -1,9 +1,6 @@
 #include "flutter_blue_plus_windows_plugin.h"
 
-// This must be included before many other Windows headers.
 #include <windows.h>
-
-// For getPlatformVersion; remove unless needed for your plugin implementation.
 #include <VersionHelpers.h>
 
 #include <flutter/method_channel.h>
@@ -12,6 +9,9 @@
 
 #include <memory>
 #include <sstream>
+#include <map>
+#include <string>
+#include <vector>
 
 namespace flutter_blue_plus_windows {
 
@@ -40,20 +40,54 @@ FlutterBluePlusWindowsPlugin::~FlutterBluePlusWindowsPlugin() {}
 void FlutterBluePlusWindowsPlugin::HandleMethodCall(
     const flutter::MethodCall<flutter::EncodableValue> &method_call,
     std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-  if (method_call.method_name().compare("flutterRestart") == 0) {
-    std::ostringstream version_stream;
-    version_stream << "Windows ";
-    if (IsWindows10OrGreater()) {
-      version_stream << "10+";
-    } else if (IsWindows8OrGreater()) {
-      version_stream << "8";
-    } else if (IsWindows7OrGreater()) {
-      version_stream << "7";
-    }
-    result->Success(flutter::EncodableValue(version_stream.str()));
-  } else {
-    result->NotImplemented();
+  const std::string& method = method_call.method_name();
+
+  if (method == "setLogLevel") {
   }
+
+  if (method == "setOptions") {
+  }
+
+  if (method == "flutterRestart") {
+  }
+
+  if (method == "connectedCount") {
+  }
+
+  if (method == "isSupported") {
+  }
+
+  if (method == "getAdapterName") {
+  }
+
+  if (method == "getAdapterState") {
+  }
+
+  if (method == "turnOn") {
+  }
+
+  if (method == "turnOff") {
+  }
+
+  if (method == "startScan") {
+  }
+
+  if (method == "stopScan") {
+  }
+
+  if (method == "getSystemDevices") {
+  }
+
+  if (method == "connect") {
+  }
+
+  if (method == "disconnect") {
+  }
+
+  if (method == "discoverServices") {
+  }
+
+  result->NotImplemented();
 }
 
 }  // namespace flutter_blue_plus_windows
